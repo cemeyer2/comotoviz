21b7954c18d5f9e62937c382499dedfccb15625b
cemeyer2 <cemeyer2@illinois.edu>
2010-02-11 23:55:11 -0600

    added template for config object

---
+++ MossLocal/src/pymoss/config/config.py
@@ -0,0 +1,17 @@
+'''
+Created on Feb 11, 2010
+
+@author: Charlie Meyer <cemeyer2@illinois.edu>
+'''
+
+class Config(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
