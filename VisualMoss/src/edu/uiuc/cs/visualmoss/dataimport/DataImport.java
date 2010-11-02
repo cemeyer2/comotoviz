8f73e550e50020a18e3dfd86dac4c8f7f7b4eeab
Jon <Jon@Jon-Desktop>
2010-11-01 20:57:02 -0500

    added constants to dataimport

--- VisualMoss/src/edu/uiuc/cs/visualmoss/dataimport/DataImport.java
+++ VisualMoss/src/edu/uiuc/cs/visualmoss/dataimport/DataImport.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
-import static edu.uiuc.cs.visualmoss.dataimport.DataConstants.*;
+import static edu.uiuc.cs.visualmoss.graph.MossGraphConstants.*;
 
 public class DataImport 
 {
