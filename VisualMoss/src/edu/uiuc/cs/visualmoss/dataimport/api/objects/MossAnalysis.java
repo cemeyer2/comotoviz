8d249ca3cdff523e10aff6d2173e7062a8627fae
Jon <Jon@Jon-Desktop>
2010-11-04 01:27:15 -0500

    Transformed MossAnalysis into refreshable, caching entities

--- VisualMoss/src/edu/uiuc/cs/visualmoss/dataimport/api/objects/MossAnalysis.java
+++ VisualMoss/src/edu/uiuc/cs/visualmoss/dataimport/api/objects/MossAnalysis.java
@@ -1,5 +1,7 @@
 package edu.uiuc.cs.visualmoss.dataimport.api.objects;
 
+import edu.uiuc.cs.visualmoss.dataimport.api.CoMoToAPI;
+import edu.uiuc.cs.visualmoss.dataimport.api.CoMoToAPIConnection;
 import edu.uiuc.cs.visualmoss.dataimport.api.CoMoToAPIReflector;
 
 import java.util.ArrayList;
@@ -14,16 +16,37 @@ import static edu.uiuc.cs.visualmoss.dataimport.api.CoMoToAPIConstants.MATCHES;
  *
  * <p> <p> Holds the data of a MOSS analysis
  */
-public class MossAnalysis {
+public class MossAnalysis implements Refreshable{
 
+    /**
+     * A unique id for this moss analysis object
+     */
     private int id;
+
+    /**
+     * A unique id for the associated analysis
+     */
     private int analysisId;
+
+    /**
+     * The associated analysis
+     */
+    private Analysis analysis;
+
+    /**
+     * A list of the matches for this moss analysis
+     */
     private List<MossMatch> matches;
 
-    public MossAnalysis() {
-    }
+    /**
+     * A connection to the API used for lazily loading components of this object and refreshing
+     */
+    private CoMoToAPIConnection connection;
 
-    public MossAnalysis(Map<String, Object> abstractMossAnalysis) {
+    public MossAnalysis(Map<String, Object> abstractMossAnalysis, CoMoToAPIConnection connection) {
+
+        //Store this connection
+        this.connection = connection;
 
         //Explicitly add the non-primitive types
         Map[] abstractMatches = (Map[]) abstractMossAnalysis.get(MATCHES);
@@ -37,6 +60,36 @@ public class MossAnalysis {
 
         CoMoToAPIReflector<MossAnalysis> reflector = new CoMoToAPIReflector<MossAnalysis>();
         reflector.populate(this, abstractMossAnalysis);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void refresh() {
+
+        //First, grab the new object from the API
+        MossAnalysis newMossAnalysis = CoMoToAPI.getMossAnalysis(connection, id);
+
+        //Copy the primitive data over
+        analysisId = newMossAnalysis.getAnalysisId();
+        matches = newMossAnalysis.getMatches();
+
+        //Clear cached data
+        analysis = null;
+    }
+
+    /**
+     * Gets the associated analysis lazily
+     *
+     * @return The analysis object
+     */
+    public Analysis getAnalysis() {
+
+        //If it's not cached, grab it from the API
+        if(analysis == null){
+            analysis = CoMoToAPI.getAnalysis(connection, analysisId);
+        }
+        return analysis;
     }
 
     public int getId() {
