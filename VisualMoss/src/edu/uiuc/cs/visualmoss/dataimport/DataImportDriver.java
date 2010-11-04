9d9f3d68e629a444e0303e03a34481449c9fa6b8
Jon <Jon@Jon-Desktop>
2010-11-03 23:44:43 -0500

    Added documentation

--- VisualMoss/src/edu/uiuc/cs/visualmoss/dataimport/DataImportDriver.java
+++ VisualMoss/src/edu/uiuc/cs/visualmoss/dataimport/DataImportDriver.java
@@ -1,9 +1,14 @@
 package edu.uiuc.cs.visualmoss.dataimport;
 
-import java.sql.SQLException;
-
 import prefuse.data.io.DataIOException;
 
+import java.sql.SQLException;
+
+/**
+ * A handle on the data import class to test its creation.
+ *
+ * @author jon
+ */
 public class DataImportDriver {
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException, DataIOException
 	{
