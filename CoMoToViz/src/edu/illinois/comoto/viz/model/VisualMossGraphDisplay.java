2d6475c3d3b9d9287e6e34008151ff95b7c271db
Jon <Jon@Jon-Desktop>
2011-03-06 21:53:59 -0600

    Added documentation to VisualMossGraphDisplay

--- CoMoToViz/src/edu/illinois/comoto/viz/model/VisualMossGraphDisplay.java
+++ CoMoToViz/src/edu/illinois/comoto/viz/model/VisualMossGraphDisplay.java
@@ -56,7 +56,6 @@ import prefuse.render.LabelRenderer;
 import prefuse.util.ColorLib;
 import prefuse.util.force.ForceSimulator;
 import prefuse.visual.NodeItem;
-import prefuse.visual.VisualGraph;
 import prefuse.visual.VisualItem;
 
 import java.awt.geom.Point2D;
@@ -73,30 +72,47 @@ import java.util.List;
  * @author chuck
  */
 public class VisualMossGraphDisplay {
+
+    // Components for displaying the graph
     private VisualMossGraph graph;
     private Display display;
-    private Visualization vis;
+    private Visualization visualization;
     private List<String> actions;
     private VisualMossVisibilityPredicate predicate;
     private VisualMossGraphDisplayContainer parent;
     private LabelRenderer labelRenderer;
     private boolean anonymous;
 
+    /**
+     * Displays the graph given a non-empty graph
+     *
+     * @param graph     The graph to display
+     * @param parent    The parent window
+     */
     protected VisualMossGraphDisplay(VisualMossGraph graph, VisualMossGraphDisplayContainer parent) {
         this.graph = graph;
         this.parent = parent;
-        init();
+        initialize();
     }
 
+    /**
+     * Displays an empty panel where the graph should be
+     *
+     * @param parent    The parent window
+     */
     protected VisualMossGraphDisplay(VisualMossGraphDisplayContainer parent) {
         this.parent = parent;
         this.graph = null;
-        init();
+        initialize();
     }
 
-    private void init() {
-        boolean hasGraph = graph != null;
+    /**
+     * Helper function to initialize this display
+     */
+    private void initialize() {
 
+        // Set default flags on this graph
+        boolean hasGraph = graph != null;
         double minimumWeightToDisplay = 0;
         boolean showSingletons = true;
         boolean showSolution = true;
@@ -105,38 +121,49 @@ public class VisualMossGraphDisplay {
         anonymous = false;
         actions = new LinkedList<String>();
 
-        vis = new Visualization();
-        if (hasGraph) {
-            VisualGraph vgraph = vis.addGraph(BackendConstants.GRAPH, graph.getPrefuseGraph());
-        }
-        vis.setInteractive(BackendConstants.GRAPH + "." + BackendConstants.EDGES, null, true);
 
+        // Add the graph itself, and make it interactive
+        visualization = new Visualization();
+        if (hasGraph) {
+            visualization.addGraph(BackendConstants.GRAPH, graph.getPrefuseGraph());
+        }
+        visualization.setInteractive(BackendConstants.GRAPH + "." + BackendConstants.EDGES, null, true);
+
+        // Initialize the colors, and run the layout engine
         setRenderers();
         setColors(BackendConstants.COLOR);
         setLayout(BackendConstants.LAYOUT, 5000); //run the layout engine for 5 seconds
 
+        // Set visibility of nodes
         predicate = new VisualMossVisibilityPredicate(minimumWeightToDisplay, showSingletons, showSolution, includePast, includePartners);
 
-        display = new Display(vis, predicate);
+        // Add the controls to this page and the listeners
+        display = new Display(visualization, predicate);
         display.addControlListener(new VisualMossControl(parent));
         display.setHighQuality(true);
-
         addControls();
     }
 
+    /**
+     * Add the renderers for the graph
+     */
     private void setRenderers() {
         labelRenderer = new LabelRenderer(BackendConstants.NETID);
         labelRenderer.setRoundedCorner(8, 8); // round the corners on nodes
-        vis.setRendererFactory(new DefaultRendererFactory(labelRenderer, new EdgeRenderer(Constants.EDGE_TYPE_CURVE)));
+        visualization.setRendererFactory(new DefaultRendererFactory(labelRenderer, new EdgeRenderer(Constants.EDGE_TYPE_CURVE)));
     }
 
+    /**
+     * Helper function to set the colors on the graph (nodes & edges)
+     */
     private void setColors(String actionName) {
-        //colors for coloring nodes, white background for regular nodes, red background for solution
-        //evals based off of isSolution, which is either "false" or "true", since "true" is alphabetically
-        //after "false", red is used for it
+
+        // colors for coloring nodes, white background for regular nodes, red background for solution
+        // evals based off of isSolution, which is either "false" or "true", since "true" is alphabetically
+        // after "false", red is used for it
         VisualMossNodeFillColorAction fill = new VisualMossNodeFillColorAction(BackendConstants.GRAPH + "." + BackendConstants.NODES, VisualItem.FILLCOLOR);
 
-        //black lines for nodes
+        // Black outlines for nodes
         VisualMossNodeStrokeColorAction stroke = new VisualMossNodeStrokeColorAction(BackendConstants.GRAPH + "." + BackendConstants.NODES, VisualItem.STROKECOLOR);
 
         // use black for node text
@@ -145,31 +172,38 @@ public class VisualMossGraphDisplay {
         // use light grey for edges
         VisualMossEdgeStrokeColorAction edges = new VisualMossEdgeStrokeColorAction(BackendConstants.GRAPH + "." + BackendConstants.EDGES, VisualItem.STROKECOLOR);
         ColorAction hl = new ColorAction(BackendConstants.GRAPH + "." + BackendConstants.NODES, VisualItem.HIGHLIGHT, ColorLib.rgb(255, 200, 125));
+
+        // Add the colors
         ActionList color = new ActionList();
         color.add(fill);
         color.add(stroke);
         color.add(text);
         color.add(edges);
         color.add(hl);
-        vis.putAction(actionName, color);
+        visualization.putAction(actionName, color);
         actions.add(actionName);
 
-        //put verdana as font
-        vis.putAction(BackendConstants.FONT, new FontAction(BackendConstants.GRAPH, BackendConstants.NODE_LABEL_FONT));
+        // Put verdana as font
+        visualization.putAction(BackendConstants.FONT, new FontAction(BackendConstants.GRAPH, BackendConstants.NODE_LABEL_FONT));
         actions.add(BackendConstants.FONT);
     }
 
+    /**
+     * Helper function for interacting with Prefuse engine
+     */
     private void setLayout(String actionName, long time) {
         ActionList layout = new ActionList(time);
         ForceDirectedLayout l = new ForceDirectedLayout(BackendConstants.GRAPH);
-        //set springs here
         ForceSimulator sim = l.getForceSimulator();
         layout.add(l);
         layout.add(new RepaintAction());
-        vis.putAction(actionName, layout);
+        visualization.putAction(actionName, layout);
         actions.add(actionName);
     }
 
+    /**
+     * Add the control listeners for this graph
+     */
     private void addControls() {
         display.addControlListener(new DragControl());
         display.addControlListener(new PanControl());
@@ -179,7 +213,7 @@ public class VisualMossGraphDisplay {
     }
 
     /**
-     * gets the Display for this vis
+     * gets the Display for this visualization
      *
      * @param width  the desired width of the display
      * @param height the desired height of the display
@@ -191,37 +225,45 @@ public class VisualMossGraphDisplay {
     }
 
     /**
-     * runs all actions associated with this vis, including coloring and layout
+     * runs all actions associated with this visualization, including coloring and layout
      */
     public void run() {
         for (String action : actions) {
-            vis.run(action);
+            visualization.run(action);
         }
     }
 
     /**
-     * runs a specific action for this vis if it exists, otherwise does nothing
+     * runs a specific action for this visualization if it exists, otherwise does nothing
      *
      * @param action the action name to run
      */
     public void run(String action) {
         if (actions.contains(action))
-            vis.run(action);
+            visualization.run(action);
     }
 
-    public VisualMossGraph getGraph() {
-        return graph;
-    }
-
+    /**
+     * Sets the new graph to be displayed, and reruns the layout engine
+     *
+     * @param graph The new graph to display
+     */
     public void setGraph(VisualMossGraph graph) {
         System.out.println("Starting to Change " + BackendConstants.GRAPH);
-        vis.removeGroup(BackendConstants.GRAPH);
+        visualization.removeGroup(BackendConstants.GRAPH);
         System.out.println("Removed Old " + BackendConstants.GRAPH + ", Adding New");
-        vis.addGraph(BackendConstants.GRAPH, graph.getPrefuseGraph());
+        visualization.addGraph(BackendConstants.GRAPH, graph.getPrefuseGraph());
         System.out.println(BackendConstants.GRAPH + " Changed");
         this.graph = graph;
         run();
         System.out.println("Run Complete");
+    }
+
+    /**
+     * Simple getter
+     */
+    public VisualMossGraph getGraph() {
+        return graph;
     }
 
     /**
@@ -235,9 +277,9 @@ public class VisualMossGraphDisplay {
     }
 
     /**
-     * sets the minimum edge weight to display
+     * Sets the minimum edge weight to display, and repaints the canvas to refresh the changes
      *
-     * @param weight
+     * @param weight The new minimum edge weight
      */
     public void setMinimumEdgeWeightToDisplay(double weight) {
         this.predicate.setMinWeight(weight);
@@ -245,15 +287,20 @@ public class VisualMossGraphDisplay {
     }
 
     /**
-     * shows or hides the solution
+     * Show or hide the solution(s) node(s)
      *
-     * @param showSolutions
+     *  @param showSolutions Whether or not to show the solution nodes
      */
     public void setShowSolution(boolean showSolutions) {
         this.predicate.setShowSolution(showSolutions);
         repaint();
     }
 
+    /**
+     * Show or hide partner edges
+     *
+     * @param includePartners   Whether or not to include partner edges
+     */
     public void setIncludePartners(boolean includePartners) {
         this.predicate.setIncludePartners(includePartners);
         repaint();
@@ -270,18 +317,18 @@ public class VisualMossGraphDisplay {
     }
 
     /**
-     * gets the current zoom scale factor
+     * Gets the current zoom scale factor
      *
-     * @return
+     * @return The zoom factor
      */
     public double getZoom() {
         return this.display.getScale();
     }
 
     /**
-     * rotates the display by the number of radians supplied
+     * Rotates the display by the number of radians supplied
      *
-     * @param radians
+     * @param radians The amount to rotate
      */
     public void rotate(double radians) {
         this.display.rotate(new Point2D.Double(display.getWidth() / 2, display.getHeight() / 2), radians);
@@ -289,18 +336,18 @@ public class VisualMossGraphDisplay {
     }
 
     /**
-     * gets the running frame rate for the viz
+     * Gets the running frame rate for the viz
      *
-     * @return
+     * @return The average frame rate
      */
     public double getAverageFrameRate() {
         return this.display.getFrameRate();
     }
 
     /**
-     * sets the display to show nodes as anonymous or not
+     * Sets the display to show nodes as anonymous or not
      *
-     * @param anonymous
+     * @param anonymous Whether or not this graph is anonymouse
      */
     public void setAnonymous(boolean anonymous) {
         if (anonymous) {
@@ -313,9 +360,9 @@ public class VisualMossGraphDisplay {
     }
 
     /**
-     * returns if we are showing anonymous or not
+     * Returns if we are showing anonymous or not
      *
-     * @return
+     * @return Whether this graph is anonymous
      */
     public boolean isAnonymous() {
         return this.anonymous;
@@ -324,8 +371,8 @@ public class VisualMossGraphDisplay {
     /**
      * writes out the current visible portion of the viz in the display to file as a png
      *
-     * @param outFile
-     * @throws IOException
+     * @param outFile   The file to which to write the image of the graph
+     * @throws IOException  On error writing to disk
      */
     public void writeToImage(File outFile) throws IOException {
         FileOutputStream os = new FileOutputStream(outFile);
@@ -333,32 +380,43 @@ public class VisualMossGraphDisplay {
     }
 
     /**
-     * forces an entire repaint on the display by marking the whole display as damaged then calling a repaint
+     * Forces an entire repaint on the display by marking the whole display as damaged then calling a repaint
      */
     public void repaint() {
         this.display.damageReport();
         this.display.repaint();
     }
 
+    /**
+     * Sets whether  past students should be included, and reruns the layout engine
+     *
+     * @param includePast   Whether or not to include past students
+     */
     public void setIncludePast(boolean includePast) {
         this.predicate.setIncludePast(includePast);
         this.repaint();
         setGraph(graph);
     }
 
+    /**
+     * Gets an iterator to navigate through the nodes of the graph
+     *
+     * @return  An iterator for the nodes of the graph
+     */
     public Iterator<NodeItem> getNodes() {
-        if (vis == null) return null;
-        return vis.items(BackendConstants.GRAPH + "." + BackendConstants.NODES);
+        if (visualization == null) return null;
+        return visualization.items(BackendConstants.GRAPH + "." + BackendConstants.NODES);
     }
 
     /**
      * animates a pan to a node with the netid supplied, if it exists in the viz. the speed of the
      * animation is given by the number of milliseconds the pan should take
      *
-     * @param netid
+     * @param netid     The netid of the node to which to pan
+     * @param lengthMS  The number of milliseconds to take to pan
      */
     public void panToNode(String netid, int lengthMS) {
-        Iterator<NodeItem> iter = vis.items(BackendConstants.GRAPH + "." + BackendConstants.NODES);
+        Iterator<NodeItem> iter = visualization.items(BackendConstants.GRAPH + "." + BackendConstants.NODES);
         NodeItem node = null;
         while (iter.hasNext()) {
             NodeItem temp = iter.next();
@@ -375,6 +433,11 @@ public class VisualMossGraphDisplay {
         display.animatePanToAbs(new Point2D.Double(x, y), lengthMS);
     }
 
+    /**
+     * Get the URL for the report (the link to view an analysis in the web browser)
+     *
+     * @return  The URL
+     */
     public String getReportURL() {
         Edge edge = (Edge) graph.getPrefuseGraph().edges().next();
         String link = edge.getString(BackendConstants.LINK);
